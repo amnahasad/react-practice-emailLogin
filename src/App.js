23e916f204f1ddf121577d9aa776bfcd32395bdf
asad0010 <asad0010@algonquinlive.com>
2021-05-14 15:49:52 -0400

    Using use-effect for typing username and password to login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+  //This will run after every component evaluation and will only run once when the app starts, when there's no dependencies (login)
+  useEffect(() => {
+    const storedLoginInfo = localStorage.getItem('isLoggedIn');
+
+    if(setIsLoggedIn === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    //storage mechanism built into the browser, 1 means logged in; 0 means not logged in
+    localStorage.setItem('isLogedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
